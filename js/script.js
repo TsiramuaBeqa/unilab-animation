7ac22f50f98c7db74f7ba31350d19595a3488411
TsiramuaBeqa <110886129+TsiramuaBeqa@users.noreply.github.com>
2022-09-02 09:22:53 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const beqaObj = {
+  firstName: "Beqa",
+  lastName: "Tsiramua",
+  age: 21,
+  married: false,
+  inrolled: ["Unilab", "It Step Georgia"],
+  university: "Ilia State University",
+  skills: ["Html", "css", "bootstrap", "JavaScript", "jQuery", "Angular", "Python"]
+}
+
+console.log(beqaObj.firstName, beqaObj.lastName, beqaObj.age, beqaObj.married, beqaObj.inrolled, beqaObj.university, beqaObj.skills)
